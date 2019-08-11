0b0ebe3705fd54644f744325aa07b6d6ed4b4cec
liangdada123 <2937589293@qq.com>
2019-08-11 12:33:55 +0800

    idea和git的整合2

--- y_news_dao/src/main/java/com/zb/dao/RpcNewsdetailMapper.java
+++ y_news_dao/src/main/java/com/zb/dao/RpcNewsdetailMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 /*
-git config --global user.name 'liangdada123d'
+git config --global user.name 'liangdada123d沙发斯蒂芬斯蒂芬'
  */
 @Mapper
 public interface RpcNewsdetailMapper {
